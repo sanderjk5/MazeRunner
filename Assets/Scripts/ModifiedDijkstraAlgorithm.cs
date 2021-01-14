ab21e9324d0723ea653a5279d02d2bf80604b836
Robin Hagmayer <robin.hagmayer@gmail.com>
2021-01-14 10:54:37 +0100

    Implemented properties and constructor

--- Assets/Scripts/ModifiedDijkstraAlgorithm.cs
+++ Assets/Scripts/ModifiedDijkstraAlgorithm.cs
@@ -5,5 +5,37 @@ using Priority_Queue;
 
 public class ModifiedDijkstraAlgorithm : MonoBehaviour
 {
-    
+    // Fields
+    private NodeController startNode;
+    private NodeController endNode;
+    private SimplePriorityQueue<HelperNodeController> prioQueue;
+    private int[,] allDistances;
+
+    // Properties
+    private int ShortestDistance { get; }
+    private List<NodeController> ShortestPath { get; }
+
+    // Constructor which needs a start node and an end node for Dijkstra's algorithm
+    public ModifiedDijkstraAlgorithm(NodeController node0, NodeController node1)
+    {
+        startNode = node0;
+        endNode = node1;
+        prioQueue = new SimplePriorityQueue<HelperNodeController>();
+        InitializeAllDistances();
+        ShortestDistance = 0;
+        ShortestPath = new List<NodeController>();
+    }
+
+    // Initialize all distances of the nodes
+    private void InitializeAllDistances()
+    {
+        allDistances = new int[MainScript.AllNodes.Count, MainScript.NumberOfNodes];
+        for(int i = 0; i < MainScript.AllNodes.Count; i++)
+        {
+            for(int j = 0; j < MainScript.NumberofStates; j++)
+            {
+                allDistances[i, j] = int.MaxValue;
+            }
+        }
+    }
 }
