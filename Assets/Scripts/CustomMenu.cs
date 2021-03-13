9549513f84de08efe3fe23edc15252a335f02ee1
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2021-03-13 18:18:38 +0100

    Updated Main Menu

---
+++ Assets/Scripts/CustomMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomMenu : MonoBehaviour
+{
+    /**
+     * <summary>Starts the normal game modus.</summary>
+     */
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
