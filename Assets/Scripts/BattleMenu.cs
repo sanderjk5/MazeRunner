dac7b96f7e5d8d45176a78375bb2302b7cb226a8
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2021-04-29 16:34:52 +0200

    Added BattleMenu

---
+++ Assets/Scripts/BattleMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleMenu : MonoBehaviour
+{
+    /**
+     * <summary>Starts the battle game mode.</summary>
+     */
+    public void StartGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void UseShooter()
+    {
+
+    }
+}
